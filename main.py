bd5aa7a4db2b9652ab4a519b723a995cde870e9d
sacha schutz <sacha@labsquare.org>
2019-07-06 17:52:30 +0200

    encapsulate code into class

--- main.py
+++ main.py
@@ -6,68 +6,106 @@ from PySide2.QtGui import *
 from PySide2.QtCharts import QtCharts as qtc
 import sys 
 from scipy.signal import find_peaks
+from scipy.stats import linregress
 import numpy as np 
 
-record = SeqIO.read('test.fsa', 'abi')
-
-data = record.annotations["abif_raw"]["DATA4"]
 
 
-scales = [35, 50, 75, 100, 139, 150, 160, 200, 250, 300, 340, 350, 400, 450, 490 , 500 ]
+class PeaksModel(object):
+    def __init__(self):
+        self.scales = [35, 50, 75, 100, 139, 150, 160, 200, 250, 300, 340, 350, 400, 450, 490 , 500 ]
 
-peaks = find_peaks(data, height=80, distance=20)[0]
+    def find_peaks(self, data):
+        peaks = find_peaks(data, height=80, distance=20)[0]
+        l_scales = len(self.scales)
+        l_peaks = len(peaks)
+        diff = l_peaks - l_scales 
+        self.peaks = peaks[diff:]
+        self.slope, self.intercept, self.r_value, self.p_value, self.std_err = linregress(self.peaks,self.scales)
 
-predicted = np.diff(np.array(scales)) 
-predicted = predicted / np.sum(predicted)
 
-errors = []
-for i in range(0,len(peaks) - len(scales)):
-    observed = np.diff(np.array(peaks[i:len(scales)+i]))
-    observed = observed / np.sum(observed)
-    error = np.sum(np.abs(predicted - observed))
-    errors.append(error)
+    def predict(self, value: float):
+        return value * self.slope + self.intercept
+
+
+class FragmentWidget(qtc.QChartView):
+    def __init__(self, filename, parent = None):
+        super().__init__(parent)
+        self.filename = filename
+        self.record = SeqIO.read(self.filename, 'abi')
+        self.build()
+
+    def build(self):
+        rox_data = self.record.annotations["abif_raw"]["DATA4"]
+        wt_data = self.record.annotations["abif_raw"]["DATA1"]
+        mt_data = self.record.annotations["abif_raw"]["DATA2"]
+        self.model =  PeaksModel()
+        self.model.find_peaks(rox_data)
+
+        self.chart = qtc.QChart()
+        self.rox_series = qtc.QLineSeries()
+        self.rox_series.setColor(Qt.red)
+
+        self.wt_series = qtc.QLineSeries()
+        self.wt_series.setColor(QColor("#4e9a06"))
     
+        self.mt_series = qtc.QLineSeries()
+        self.mt_series.setColor(QColor("#27A4DD"))
+    
+        for x, y in enumerate(rox_data):
+            self.rox_series.append(self.model.predict(x),y)
 
-start = errors.index(min(errors))
+        for x, y in enumerate(wt_data):
+            self.wt_series.append(self.model.predict(x),y)
 
-peaks = peaks[len(peaks)-len(scales):]
+        for x, y in enumerate(mt_data):
+            self.mt_series.append(self.model.predict(x),y)        
+
+
+        self.chart.addSeries(self.rox_series)
+        self.chart.addSeries(self.wt_series)
+        self.chart.addSeries(self.mt_series)
+        self.chart.createDefaultAxes()
+        self.setRubberBand(qtc.QChartView.RectangleRubberBand)
+        self.chart.axisX().setRange(0,500)
+
+        self.setChart(self.chart)
+
+
+
 
 if __name__ == "__main__":
     
     app = QApplication(sys.argv)
-
-    chart = qtc.QChart()
     
 
-
-    view  = qtc.QChartView(chart)
+    
+    view = FragmentWidget("test.fsa")
 
     view.resize(800,600)
 
-    view.setRubberBand(qtc.QChartView.RectangleRubberBand)
 
-    serie = qtc.QLineSeries()
-    serie.setPen(QPen("red"))
+    # rox_series = qtc.QLineSeries()
+    # rox_series.setPen(QPen("red"))
     
-    serie = qtc.QLineSeries()
-    serie.setPen(QPen("red"))
+    # rox_series = qtc.QLineSeries()
+    # rox_series.setPen(QPen("red"))
 
-    scatter = qtc.QScatterSeries()
-    scatter.setMarkerSize(10)
-    scatter.setMarkerShape(qtc.QScatterSeries.MarkerShapeRectangle)
+    # scatter = qtc.QScatterSeries()
+    # scatter.setMarkerSize(10)
+    # scatter.setMarkerShape(qtc.QScatterSeries.MarkerShapeRectangle)
 
-    for x in peaks:
-        scatter.append(x, 1000)
+    # for x in peaks:
+    #     scatter.append(x, 1000)
 
 
 
-    for x, y in enumerate(data):
-        serie.append(x,y)
+    # for x, y in enumerate(rox_data):
+    #     rox_series.append(x,y)
 
-    chart.addSeries(serie)
-    chart.addSeries(scatter)
+    # chart.addSeries(rox_series)
+    # chart.addSeries(scatter)
 
-    chart.createDefaultAxes()
 
 
 
